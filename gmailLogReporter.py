afbe220f78efa5ddaff6d11211324c6b1ea69795
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2024-11-02 11:38:12 -0700

    Update gmailLogReporter.py

--- gmailLogReporter.py
+++ gmailLogReporter.py
@@ -1,17 +1,28 @@
 from libraries.google.parser import GoogleLogParser
 from libraries.google.reporter import Reporter
 from argparse import ArgumentParser
+
 LOG_PARSER = GoogleLogParser()
 REPORTER = Reporter()
 
 def main():
-    parser = ArgumentParser(description="Generates a report from the provided Google Log Search Export")
-    parser.add_argument("--docx", type=str, help="The file to store the report")
-    parser.add_argument("--logfile", type=str, help="The Google Log Search Export to load")
+    parser = ArgumentParser(description="Generate a detailed report based on a Google Log Search Export file.")
+    parser.add_argument(
+        "--docx", 
+        type=str, 
+        required=True,
+        help="Path to the DOCX file where the generated report will be saved."
+    )
+    parser.add_argument(
+        "--logfile", 
+        type=str, 
+        required=True,
+        help="Path to the Google Log Search Export file to be processed."
+    )
     args = parser.parse_args()
+    
     LOG_PARSER.read_export(args.logfile)
     REPORTER.generate_report(args.docx, LOG_PARSER.get_entries())
-
 
 if __name__ == "__main__":
     main()
